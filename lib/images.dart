b8bab8634ae2d6bea72c1e3f46a0a3e216725d39
mohammad tahourian <mhdtahourian@gmail.com>
2022-12-12 10:49:19 +0330

    project completed

---
+++ lib/images.dart
@@ -0,0 +1,12 @@
+class Images {
+  static List<String> images = [
+    'https://newcdn.fidibo.com/images/books/135684_11806_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/135375_80516_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/136068_15784_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/135679_39172_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/136692_51241_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/136159_76305_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/136690_95733_normal.jpg?width=200',
+    'https://newcdn.fidibo.com/images/books/135431_42461_normal.jpg?width=200',
+  ];
+}
---
+++ lib/main_screen.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:fb_task/images.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> with TickerProviderStateMixin {
+  final int squareSpeed = 2;
+  final double squareSize = 160;
+  final int reoccurTime = 100;
+  int imageIndex = 0;
+  late AnimationController tweenController;
+  late double _bottomPosition =
+      (MediaQuery.of(context).size.height / 2) - (squareSize / 2);
+  late double _rightPosition =
+      (MediaQuery.of(context).size.width / 2) - (squareSize / 2);
+  late int _screenHeight;
+  late int _screenWidth;
+  late String squareDirection;
+
+  @override
+  void initState() {
+    super.initState();
+
+    tweenController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..addListener(() {
+        Timer.periodic(
+          Duration(milliseconds: reoccurTime),
+          (timer) {
+            switch (squareDirection) {
+              case 'bottom':
+                if (_bottomPosition >= 0) {
+                  setState(() {
+                    _bottomPosition -= squareSpeed;
+                  });
+                  if (_bottomPosition == 0) {
+                    squareDirection = 'up';
+                  }
+                }
+                break;
+              case 'up':
+                if (_bottomPosition <= _screenHeight - squareSize) {
+                  setState(() {
+                    _bottomPosition += squareSpeed;
+                  });
+                  if (_bottomPosition == _screenHeight - squareSize) {
+                    squareDirection = 'bottom';
+                  }
+                }
+                break;
+
+              case 'right':
+                if (_rightPosition >= 0) {
+                  setState(() {
+                    _rightPosition -= squareSpeed;
+                  });
+                  if (_rightPosition == 0) {
+                    squareDirection = 'left';
+                  }
+                }
+                break;
+              case 'left':
+                if (_rightPosition <= _screenWidth) {
+                  setState(() {
+                    _rightPosition += squareSpeed;
+                  });
+                  if (_rightPosition == _screenWidth - squareSize) {
+                    squareDirection = 'right';
+                  }
+                }
+                break;
+
+              default:
+            }
+          },
+        );
+      });
+  }
+
+  @override
+  void dispose() {
+    tweenController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaSize = MediaQuery.of(context).size;
+    _screenHeight = mediaSize.height.toInt();
+    _screenWidth = mediaSize.width.toInt();
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: _bottomPosition,
+            left: _rightPosition,
+            child: GestureDetector(
+              onHorizontalDragEnd: (deg) {
+                if (deg.primaryVelocity! > 0) {
+                  squareDirection = 'left';
+                } else if (deg.primaryVelocity! < 0) {
+                  squareDirection = 'right';
+                }
+                tweenController.forward();
+              },
+              onVerticalDragEnd: ((deg) {
+                if (deg.primaryVelocity! > 0) {
+                  squareDirection = 'bottom';
+                } else if (deg.primaryVelocity! < 0) {
+                  squareDirection = 'up';
+                }
+                tweenController.forward();
+              }),
+              onTap: () {
+                final random = Random();
+                setState(() {
+                  imageIndex = random.nextInt(Images.images.length);
+                });
+              },
+              child: Container(
+                width: squareSize,
+                height: squareSize,
+                decoration: BoxDecoration(
+                  color: const Color(0xff28c5cc),
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(Images.images[imageIndex]),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
